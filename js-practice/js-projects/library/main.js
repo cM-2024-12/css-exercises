ca9507be33e6baadb4387c3081a79ed56fdcec10
Christian Merrick <192653753+cM-2024-12@users.noreply.github.com>
2025-12-07 13:35:06 -0500

    Fix All

--- js-practice/js-projects/library/main.js
+++ js-practice/js-projects/library/main.js
@@ -15,40 +15,59 @@ function Book(title, author, pages, read) {
   };
 }
 
+Book.prototype.toggleRead = function() { 
+    this.read = !this.read; // toggles other value? 
+}
+
 function addBookToLibrary(book) {
   myLibrary.push(book)
 }
 
-// function displayLibrary() {
-//   myLibrary.forEach(book => {
-//     console.log(book.info());
-//     // OR create DOM elements to display each book
-//   });
-// }
 
 const theHobbit = new Book('The Hobbit', 'JR', 295, true)  // Added missing params
 const standByMe = new Book('Stand By Me', 'McCluskey', 192, false)
 
-
-
 const bookContainer = document.querySelector("div.book-container")
+const closeButton = document.querySelector("dialog button")
+const dialog = document.querySelector("dialog");
+const addBook = document.querySelector("#new-book")
 
 function returnLibrary(){
-  bookContainer.innerHtml = '' // clear container each time
+  bookContainer.innerHTML = '' // clear container each time
   myLibrary.forEach(book => {
     const bookInfo = `${book.title}, written by ${book.author}. ${book.pages}. ${book.read}`
     const bookNode = document.createElement("div");
     bookNode.textContent = bookInfo
+    bookNode.dataset.bookId = book.id
+
+    const deleteBtn = document.createElement("button")
+    deleteBtn.className = "delete-btn"
+    deleteBtn.textContent = "Delete"
+    deleteBtn.addEventListener("click", (e) => {
+      console.log(e.target)
+      console.log(e.target.parentElement)
+      const bookId = e.target.parentElement.dataset.bookId
+      const index = myLibrary.findIndex(book => book.id === bookId)
+      myLibrary.splice(index, 1);
+      returnLibrary();
+    }); 
+
+    const toggleBtn = document.createElement("button")
+    toggleBtn.className = "toggle-btn"
+    toggleBtn.textContent = "Read"
+    toggleBtn.addEventListener("click", (e) => {
+      const bookId = e.target.parentElement.dataset.bookId
+      const book = myLibrary.find(book => book.id === bookId)
+      book.toggleRead();
+      returnLibrary();
+    })
+
+    bookNode.appendChild(deleteBtn)
+    bookNode.appendChild(toggleBtn)
     bookContainer.appendChild(bookNode)
   })
 }
 
-
-const closeButton = document.querySelector("dialog button")
-const dialog = document.querySelector("dialog");
-const addBook = document.querySelector("#new-book");
-
-// "Show the dialog" button opens the dialog modally
 addBook.addEventListener("click", () => {
   dialog.showModal();
 });
