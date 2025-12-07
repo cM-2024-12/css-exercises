622cec9244935917fe04d4881de3d277e4274d41
Christian Merrick <192653753+cM-2024-12@users.noreply.github.com>
2025-12-07 08:26:16 -0500

    Libary Work

---
+++ js-practice/js-projects/library/main.js
@@ -0,0 +1,49 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("Must use new")
+    }
+    this.id = crypto.randomUUID()
+    this.title = title 
+    this.author = author
+    this.pages = pages
+    this.read = read
+    this.info = function() {
+      const readStatus = this.read ? "already read" : "not read yet";
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+  };
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book)
+  // take params, create a book then store it in the array
+}
+
+const theHobbit = new Book('The Hobbit', 'JR', 295, true)  // Added missing params
+const standByMe = new Book('Stand By Me', 'McCluskey', 192, false)
+
+addBookToLibrary(theHobbit)
+addBookToLibrary(standByMe)
+
+function returnLibrary(){
+  myLibrary.forEach(book => {
+    return book
+  })
+}
+
+const closeButton = document.querySelector("dialog button")
+const dialog = document.querySelector("dialog");
+const addBook = document.querySelector("#new-book");
+
+// "Show the dialog" button opens the dialog modally
+addBook.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+  dialog.close();
+})
+
+
+// Next Step is add 
